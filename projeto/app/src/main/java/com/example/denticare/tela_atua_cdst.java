0b3de499a7c814acddb1d293ce7d22bd3fbf50f0
FelipeJoseLeite <felipe.leite@edu.unipar.br>
2023-06-10 14:16:09 -0300

    implementado telas remover cliente, atualizar cadastro de cliente e gerar pdf

---
+++ projeto/app/src/main/java/com/example/denticare/tela_atua_cdst.java
@@ -0,0 +1,14 @@
+package com.example.denticare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_atua_cdst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_atua_cdst);
+    }
+}
---
+++ projeto/app/src/main/java/com/example/denticare/tela_confir_remo_cdst.java
@@ -0,0 +1,14 @@
+package com.example.denticare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_confir_remo_cdst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_confir_remo_cdst);
+    }
+}
