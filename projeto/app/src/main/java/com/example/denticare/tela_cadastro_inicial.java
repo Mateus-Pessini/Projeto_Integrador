78daeb3b2fff06719864a3ebed56332e4674e701
FelipeJoseLeite <felipe.leite@edu.unipar.br>
2023-06-11 23:03:06 -0300

    implementado telas de login e cadastro inicial

---
+++ projeto/app/src/main/java/com/example/denticare/tela_cadastro_inicial.java
@@ -0,0 +1,13 @@
+package com.example.denticare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_cadastro_inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro_inicial);
+    }
+}
