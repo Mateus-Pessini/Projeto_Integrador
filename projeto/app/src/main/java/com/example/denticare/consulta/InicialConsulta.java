eb83eed4f5c93a3d4c7fccfcdd5c1c47f10dca0f
FelipeJoseLeite <felipe.leite@edu.unipar.br>
2023-10-07 17:18:51 -0300

    att

--- projeto/app/src/main/java/com/example/denticare/consulta/InicialConsulta.java
+++ projeto/app/src/main/java/com/example/denticare/consulta/InicialConsulta.java
@@ -1,14 +1,16 @@
-package com.example.denticare;
+package com.example.denticare.consulta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HistoricoDentario extends AppCompatActivity {
+import com.example.denticare.R;
+
+public class InicialConsulta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_historico_dentario);
+        setContentView(R.layout.activity_inicial_consulta);
     }
 }
