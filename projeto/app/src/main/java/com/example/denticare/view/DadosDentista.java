637ec42c12d5c861d86be1116ee0f777f97584f8
FelipeJoseLeite <felipe.leite@edu.unipar.br>
2023-12-14 23:16:36 -0300

    att

--- projeto/app/src/main/java/com/example/denticare/view/DadosDentista.java
+++ projeto/app/src/main/java/com/example/denticare/view/DadosDentista.java
@@ -1,12 +1,15 @@
 package com.example.denticare.view;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,10 +20,15 @@ import com.example.denticare.R;
 import com.example.denticare.api.Api.ApiEstado;
 import com.example.denticare.api.Api.ApiPessoa;
 import com.example.denticare.api.Api.RetroFit;
+import com.example.denticare.api.models.enums.TpPessoaEnum;
 import com.example.denticare.api.models.pessoa.Estado;
 import com.example.denticare.api.models.pessoa.Pessoa;
 import com.example.denticare.util.DataUtils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Base64;
 import java.util.List;
 
 import retrofit2.Call;
@@ -37,7 +45,9 @@ public class DadosDentista extends AppCompatActivity {
     private LinearLayout btCadFotoRecep;
     private LinearLayout btSair;
     private LinearLayout btCadClienteRecep;
+    private LinearLayout btMeusDados;
     private TextView tvNome;
+    private ImageView ivImgDentista;
     private EditText edNome, edFone, edMail, edCRO, edEspecialidade;
 
     @Override
@@ -63,8 +73,10 @@ public class DadosDentista extends AppCompatActivity {
         //edMail.setInputType(InputType.TYPE_NULL);
         edCRO = findViewById(R.id.edDentCro);
         edEspecialidade = findViewById(R.id.edDentEspecialidade);
+        btMeusDados = findViewById(R.id.btMeusDados);
+        ivImgDentista = findViewById(R.id.ivImgDentista);
 
-        tvNome.setText(DataUtils.getDataFromTokenToShow(DadosDentista.this, "name"));
+        buscaTipoUsuario();
 
         String token = DataUtils.getToken(DadosDentista.this);
         ApiPessoa apiPessoa = RetroFit.REGISTER_PESSOA();
@@ -197,5 +209,37 @@ public class DadosDentista extends AppCompatActivity {
             }
         });
 
+    }
+    private void buscaTipoUsuario(){
+        SharedPreferences sharedPreferences = getSharedPreferences("MyToken", Context.MODE_PRIVATE);
+        String token = sharedPreferences.getString("token", "");
+        String role = "";
+        if (!token.isEmpty()) {
+            Base64.Decoder decoder = Base64.getUrlDecoder();
+            String[] tokenSplited = token.split("\\.");
+            String header = new String(decoder.decode(tokenSplited[0]));
+            String payload = new String(decoder.decode(tokenSplited[1]));
+            String name;
+            try {
+                name = new JSONObject(payload).getString("Name");
+                role = new JSONObject(payload).getString("Role");
+            } catch (JSONException e) {
+                name = "";
+            }
+            tvNome.setText(name);
+
+            if (role.equals(TpPessoaEnum.DENTISTA.toString())) {
+                btCadClienteRecep.setVisibility(View.GONE);
+                btAgendarRecep.setVisibility(View.GONE);
+                Log.d("TipoUsuario", "Usuário é um Dentista");
+            } else if (role.equals(TpPessoaEnum.SECRETARIA.toString())) {
+                btMeusDados.setVisibility(View.GONE);
+                ivImgDentista.setVisibility(View.INVISIBLE);
+                //tvNomeDentista.setVisibility(View.GONE);
+                Log.d("TipoUsuario", "Usuário é uma Secretária");
+            } else {
+
+            }
+        }
     }
 }
